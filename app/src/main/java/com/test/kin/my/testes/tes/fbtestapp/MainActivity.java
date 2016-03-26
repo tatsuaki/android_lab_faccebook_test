6a27769358527b8cc5478694731d35ccd8aa5582
tatsuaki_kin <tatuaki_dear@yahoo.co.jp>
2016-03-26 12:00:19 +0900

    adds

--- app/src/main/java/com/test/kin/my/testes/tes/fbtestapp/MainActivity.java
+++ app/src/main/java/com/test/kin/my/testes/tes/fbtestapp/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+    //
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         AccessToken token = AccessToken.getCurrentAccessToken();
